fe7ab91b58e9db6fb803c6f4fa8b1c8671bdad2c
oeelvik <elvik@oneway.no>
2014-11-23 12:48:01 +0100

    Improved example code

--- examples/StopWatch/StopWatch.ino
+++ examples/StopWatch/StopWatch.ino
@@ -5,21 +5,35 @@ StopWatch stopWatch;
 
 void setup(){
 	Serial.begin(9600);
-	Serial.println(stopWatch.index);
 	stopWatch.init();
-	Serial.println(stopWatch.index);
-	Serial.println(stopWatch.splitTimes[0]);
-	Serial.println(stopWatch.splitTimes[1]);
-	Serial.println(stopWatch.splitTimes[2]);
-	Serial.println();
-
-	Serial.println(stopWatch.overhead);
 	stopWatch.start();
+	stopWatch.split();//Allways call split before the first task
+        Serial.println("First Task");
+	stopWatch.split();
+        int num = 2+4;
+	stopWatch.split();
+        Serial.print(String(num));
 	stopWatch.split();
 	stopWatch.split();
-	Serial.println(stopWatch.splitTimes[0]);
+	stopWatch.split();
+	stopWatch.split();
+	stopWatch.split();
+	stopWatch.split();
+	
+	Serial.println();
+	Serial.println("Printing 'First Task' tok:");
 	Serial.println(stopWatch.splitTimes[1]);
+	Serial.println("int num = 2+4; tok:");
 	Serial.println(stopWatch.splitTimes[2]);
+	Serial.println("Printing num tok:");
+	Serial.println(stopWatch.splitTimes[3]);
+	Serial.println("No task:");
+	Serial.println(stopWatch.splitTimes[4]);
+	Serial.println(stopWatch.splitTimes[5]);
+	Serial.println(stopWatch.splitTimes[6]);
+	Serial.println(stopWatch.splitTimes[7]);
+	Serial.println(stopWatch.splitTimes[8]);
+	Serial.println(stopWatch.splitTimes[9]);
 	Serial.println();
 }
 
